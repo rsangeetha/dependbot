a19cd90b3980066970920016e78815f99001c03e
gpbee <prakash.balagala@gmail.com>
2021-07-13 12:16:10 +0530

    Initial Commit

---
+++ src/main/java/com/demo/depboat/DepboatApplication.java
@@ -0,0 +1,13 @@
+package com.demo.depboat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepboatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepboatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/depboat/DepboatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.depboat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepboatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
